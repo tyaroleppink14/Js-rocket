5c61f8218e467828a36f906c75647d7f697b78a5
Tyaro Leppink <Tyaro.leppink@gmail.com>
2021-10-06 11:17:00 +0200

    added css and a few js changes

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ var car = {
     isTurnedOn: false,
     numberOfWheels: 4,
     seats: ["seat 1", "seat 2", "seat 3", "seat 4"],
+
+    turnOn: function () {
+        this.isTurnedOn = true;
+    },
+
+    fly: function () {
+        alert("fly");
+    },
+    switchCar: function (isOn) {
+        console.log('turn car ' + isOn);
+        if (isOn == true) {
+            this.isTurnedOn = true;
+        } else {
+            this.isTurnedOn == false;
+        }
+    },
 };
 
 console.log("Hello there ");
