271cd5753ead02a0e3b2db8b2bc4e1610ed36cfa
Tyaro Leppink <Tyaro.leppink@gmail.com>
2021-10-06 11:03:57 +0200

    added a car to the javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+var car = {
+    make: "VW",
+    type: "Polo",
+    color: "blue",
+    isTurnedOn: false,
+    numberOfWheels: 4,
+    seats: ["seat 1", "seat 2", "seat 3", "seat 4"],
+};
+
+console.log("Hello there ");
