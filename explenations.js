71e83b1e18a170b7cdab559a0602400d946f79d0
Tyaro Leppink <Tyaro.leppink@gmail.com>
2021-10-06 11:00:05 +0200

    Explenations added

---
+++ explenations.js
@@ -0,0 +1,42 @@
+//Explanations
+var fullname = function(firstname){
+    return firstname + ' ' + secondname;
+
+}
+
+var area = function(width, height){
+    return width * height;
+
+}
+
+var whatIsMyFavColor = function(){
+return false;
+
+return true;
+
+return 'blue';
+}
+
+//Explanations
+var doSomething = function(){
+console.log("Do something")
+}
+
+// if statements
+var name1 = 'Rich';
+var name2 = 'Bob';
+
+
+if (name1 == 'Rich' && name2 == 'Bob') {
+// do something
+alert('true 1');
+} else if(name2 == 'Rich'){
+    alert('true 2');
+}
+else{
+    alert('false');
+}
+
+
+
+
